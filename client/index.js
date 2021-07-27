5db107b0a6574bb80932ce59a18c88a1f778d183
Alex Fox <acfox7@gmail.com>
2021-07-27 19:04:22 -0400

    Redux Store Set-Up Incl. React Provider Component

--- client/index.js
+++ client/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<h1>Loading Has Worked</h1>, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <h1>Page Has Loaded Properly</h1>
+  </Provider>,
+  document.getElementById('app')
+);
